2f780ceacc11f84ff9abb4a083b61fd7cdb40b6a
Raqbit <raqbit@outlook.com>
2019-01-06 15:57:54 +0100

    Add migrations to startup

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,8 @@ FROM golang:1.11.1-alpine as builder
 
 RUN apk --no-cache add git
 
-COPY . $GOPATH/src/github.com/raqbit/catbot/
-WORKDIR $GOPATH/src/github.com/raqbit/catbot/
+COPY . /go/src/github.com/raqbit/catbot/
+WORKDIR /go/src/github.com/raqbit/catbot/
 
 RUN go get -d -v
 
@@ -13,8 +13,9 @@ FROM alpine:3.8
 
 RUN apk --no-cache add ca-certificates tini net-tools
 
-COPY --from=builder /go/bin/catbot /go/bin/catbot
+COPY --from=builder /go/bin/catbot /catbot
+COPY --from=builder /go/src/github.com/raqbit/catbot/migrations /migrations
 
 ENTRYPOINT ["/sbin/tini", "--"]
 
-CMD ["/go/bin/catbot"]
+CMD ["/catbot"]
--- db.go
+++ db.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"github.com/golang-migrate/migrate"
+	"github.com/golang-migrate/migrate/database/postgres"
+	_ "github.com/golang-migrate/migrate/source/file"
 	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 	"time"
 )
 
@@ -46,7 +53,86 @@ func NewDb(dataSourceName string) (*DB, error) {
 	if err = db.Ping(); err != nil {
 		return nil, err
 	}
+
+	err = migrateDatabase(db)
+
+	if err != nil {
+		logrus.WithError(err).Error("Could not migrate database")
+		return nil, err
+	}
+
 	return &DB{db}, nil
+}
+
+func migrateDatabase(db *sqlx.DB) error {
+	driver, err := postgres.WithInstance(db.DB, &postgres.Config{})
+
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://migrations",
+		"postgres", driver)
+
+	if err != nil {
+		return err
+	}
+
+	currVersion, dirty, err := getDatabaseVersion(m)
+
+	if dirty {
+		err = errors.New("database is dirty")
+	}
+
+	if err != nil {
+		logrus.WithError(err).Error("Could not get database version")
+		return err
+	}
+
+	err = m.Up()
+
+	if err == migrate.ErrNoChange {
+		logrus.WithField("version", currVersion).Info("Database up to date")
+		return nil
+	}
+
+	if err != nil {
+		return err
+	}
+
+	newVersion, dirty, err := getDatabaseVersion(m)
+
+	if dirty {
+		err = errors.New("database is dirty")
+	}
+
+	if err != nil {
+		logrus.WithError(err).Error("Could not get database version")
+		return err
+	}
+
+	logrus.WithFields(logrus.Fields{
+		"oldVersion": currVersion,
+		"newVersion": newVersion,
+	}).Info("Updated database")
+
+	return nil
+}
+
+func getDatabaseVersion(m *migrate.Migrate) (string, bool, error) {
+	version := "None"
+	ver, dirty, err := m.Version()
+
+	if err != nil && err != migrate.ErrNilVersion {
+		return "", false, err
+	}
+
+	if err == nil {
+		version = fmt.Sprint(ver)
+	}
+
+	return version, dirty, nil
 }
 
 func (db *DB) BeginTransaction() (*Tx, error) {
